1dd4c5cf5a1feadceaea1d1ceb857ffd8952097c
Ferchu98 <frnndbea@gmail.com>
2021-01-22 13:10:23 +0100

    Método para aumentar la barra de progreso con animación

--- Assets/Scripts/ProgressBar.cs
+++ Assets/Scripts/ProgressBar.cs
@@ -1,18 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ProgressBar : MonoBehaviour
 {
+
+    private Slider slider;
+
+    public float fillSpeed = 0.5f;
+    private float targetProgress = 0;
+
+    private void Awake()
+    {
+        slider = gameObject.GetComponent<Slider>();
+    }
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Increment(0.75f);
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        if (slider.value < targetProgress)
+        {
+            slider.value += fillSpeed * Time.deltaTime;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Increment(0.1f);
+        }
     }
+
+    public void Increment (float valorInc)
+    {
+        targetProgress = slider.value + valorInc;
+    }
+
 }
